aaae02d42f89f188bb610076ec10cf004f532759
john <frazao.jhonn@gmail.com>
2015-10-08 18:54:44 -0400

    Script Update

--- newsite.sh
+++ newsite.sh
@@ -39,14 +39,6 @@ block="server {
 		root /var/www/$1/html;
 	}
 
-	location ~ \.php$ {
-		try_files \$uri =404;
-		fastcgi_split_path_info ^(.+\.php)(/.+)$;
-		fastcgi_pass unix:/var/run/php5-fpm.sock;
-		fastcgi_index index.php;
-		include fastcgi_params;
-	}
-
 	# Only for nginx-naxsi used with nginx-naxsi-ui : process denied requests
 	#location /RequestDenied {
 	#	proxy_pass http://127.0.0.1:8080;
@@ -56,11 +48,12 @@ block="server {
 	# pass the PHP scripts to FastCGI server listening on 127.0.0.1:9000
 	#
 	location ~ \.php$ {
+		try_files \$uri =404;
 		fastcgi_split_path_info ^(.+\.php)(/.+)$;
 		# NOTE: You should have \"cgi.fix_pathinfo = 0;\"in php.ini
 
 		# With php5-cgi alone:
-		fastcgi_pass 127.0.0.1:9000;
+		#fastcgi_pass 127.0.0.1:9000;
 		# With php5-fpm:
 		fastcgi_pass unix:/var/run/php5-fpm.sock;
 		fastcgi_index index.php;
